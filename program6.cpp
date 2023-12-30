5819dd4b0cc602a1432fed011bf47866f1f87c6b
Garvitb2005 <142775976+Garvitb2005@users.noreply.github.com>
2023-12-30 19:58:31 +0530

    Update program6.cpp

--- program6.cpp
+++ program6.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 
 int main() {
-    float marks;
-    cout << "Enter your marks: ";
-    cin >> marks; //prints grade depending on the marks given using if else if loop
+    float a,b,c,d,e, marks;
+    cout<<"Enter marks of first subject";
+    cin>>a;
+    cout<<"Enter marks of second subject";
+    cin>>b;
+    cout<<"Enter marks of third subject";
+    cin>>c;
+    cout<<"Enter marks of fourth subject";
+    cin>>d;
+    cout<<"Enter marks of fifth subject";
+    cin>>e;
+    marks = (a+b+c+d+e)/5;
     if (marks >= 90) {
-        cout << "A";
+        cout << "Grade - A";
     } else if (marks >= 80) {
-        cout << "B";
+        cout << "Grade - B";
     } else if (marks >= 70) {
-        cout << "C";
+        cout << "Grade - C";
     } else if (marks >= 60) {
-        cout << "D";
+        cout << "Grade - D";
     } else {
-        cout << "F";
+        cout << "Grade - F";
     }
     return 0;
 }
